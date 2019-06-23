f751e17ea0fcafb9afac95883db053dc2e9297e9
kalmykov1981 <kalmikovw13@gmail.com>
2019-06-23 07:39:55 -0500

    Initial Commit

---
+++ Navigation Segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation Segue
+//
+//  Created by Сергей Калмыков on 6/23/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
